8b96a56626b5195ca2637fe342a3e3e37fd27543
Jesus Armando <armando_saenz@live.com.mx>
2020-10-20 16:13:04 -0500

    Web modular
    Se implementaron los cambios para que la pagina fuera modular al cargar cada seccion

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  });
-$(function () {
-    $('[data-toggle="popover"]').popover()
-  });
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+
   $('.popover-dismiss').popover({
     trigger: 'focus'
   });
- 
-  function modInicio() {
-    $("#contenedor").empty();
-    $("#contenedor").load("pages/inicio.html",function () {
-
-    });
-  };
+});
