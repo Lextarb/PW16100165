a8e40806c614c61e8ef53179772033cb79616fe9
Jesus Armando <armando_saenz@live.com.mx>
2020-10-20 17:24:42 -0500

    Implementacion del registro
    Basado el registro en el formulario que el profesor nos proporciono.

--- js/script.js
+++ js/script.js
@@ -5,4 +5,24 @@ $(function () {
   $('.popover-dismiss').popover({
     trigger: 'focus'
   });
+
+
+  $("#validatedCustomFile1").change(function () {
+    readURL1(this);
+  });
+  function readURL1(input) {
+    if (input.files[0].type == 'image/jpeg' || input.files[0].type == 'image/png') {
+      $.each(jQuery(input)[0].files, function (i, file) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $('#urlImage1').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      });
+    } else {
+      alert("Solo se permiten archivos .jpg y .png");
+    }
+  }
+
+
 });
